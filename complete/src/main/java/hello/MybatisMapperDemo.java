fc95827c517e07690ecedce95938dd6353cb0526
poiu72 <poiu72@163.com>
2015-10-27 15:33:40 +0800

    spring Building a RESTful Web Service with spring boot

---
+++ complete/src/main/java/hello/MybatisMapperDemo.java
@@ -0,0 +1,13 @@
+package hello;
+
+import org.mybatis.spring.annotation.MapperScan;
+
+/**
+ * Created by admin on 2015/10/27.
+ */
+@MapperScan
+public class MybatisMapperDemo {
+    public void methodTest(){
+
+    }
+}
